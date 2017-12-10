8e0992cf1cfb70a290f39396a535b726772045a8
Adam Christopher <adam.keristoper@gmail.com>
2017-12-10 16:40:45 +0900

    done with all schemas and done with all theories

--- reducers.js
+++ reducers.js
@@ -357,3 +357,101 @@ function usersDucks(state = initialState, action) {
 }
 
 // Replies
+const initialReply = {
+  name: '',
+  reply: '',
+  uid: '',
+  timestamp: 0,
+  avatar: '',
+  replyId: ''
+}
+
+function duckReplies (state = initialReply, action) {
+  switch (action.type) {
+    case "ADD_REPLY" :
+      return {
+        ...state,
+        [action.reply.replyId]: action.reply,
+      }
+    case "REMOVE_REPLY" :
+      return {
+        ...state,
+        [action.reply.replyId]: undefined,
+      }
+    default:
+      return state
+  }
+}
+const initialDuckState = {
+  lastUpdated = Date.now(),
+  replies: {}
+}
+
+function repliesAndLastUpdated(state = initialDuckState, action) {
+  switch (action.type) {
+    case "FETCHING_REPLIES_SUCCESS" :
+      return {
+        ...state,
+        lastUpdated: action.lastUpdated,
+        replies: action.replies,
+      }
+    case "ADD_REPLY" :
+    case "REMOVE_REPLY" :
+      return {
+        ...state,
+        replies: duckReplies(state[action.replies], action),
+      }
+    default:
+      return state;
+  }
+}
+
+const initialState = {
+  isFetching = true,
+  error = '',
+}
+
+function replies(state = initialState, action) {
+  switch (expression) {
+    case expression:
+    case "ADD_REPLY" :
+      return {
+        ...state,
+        [action.duckId]: {
+          lastUpdated: timestamp,
+          replies: {
+            [action.replyId]: {
+              reply: action.reply,
+            }
+          }
+        }
+      }
+    case "REMOVE_REPLY" :
+      return {
+        ...state,
+        isFetching: false,
+        error: '',
+        [action.duckId]: repliesAndLastUpdated(state[action.duckId], action),
+      }
+    case "FETCHING_REPLIES" :
+      return {
+        ...state,
+        isFetching: true,
+      }
+    case "ADD_REPLY_ERROR" :
+    case "FETCHING_REPLIES_ERROR" :
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
+      }
+    case "FETCHING_REPLIES_SUCCESS" :
+      return {
+        ...state,
+        isFetching: false,
+        error: '',
+      }
+    default:
+      return state;
+  }
+}
